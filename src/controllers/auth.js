61edae91321ede1b59023e80ba4953a12d725e4d
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-02 14:54:48 +0545

    register controller

---
+++ src/controllers/auth.js
@@ -0,0 +1,42 @@
+const { validationResult } = require("express-validator")
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+require('dotenv/config')
+const JWT_SECRET = process.env.JWT_SECRET
+
+exports.registerUser = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        //Check for dupliate email id
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            res.status(401).json({ error: "Sorry email already used" })
+        }
+
+        const salt = await bcrypt.genSalt(12);
+        const securePassword = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = await User.create({
+            fname: req.body.fname,
+            lname: req.body.lname,
+            name: req.body.fname + " " + req.body.lname,
+            email: req.body.email,
+            password: securePassword,
+        });
+        const data = {
+            user: {
+                id: newUser.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET)
+        const msg = "User created successfully"
+        res.status(201).json({ authToken, msg })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
