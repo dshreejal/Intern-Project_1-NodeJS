29bec5e8b274157c348ef6a17d452a1d1e724286
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-02 16:53:18 +0545

    articles route

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require('express-validator');
+const UserData = require('../middleware/UserData')
 
-router.get("/", (req, res) => {
-    res.send("")
-})
+const { addArticle, fetchArticle, editArticle, deleteArticle } = require("../controllers/articles")
 
-router.post("/")
+router.post("/add", UserData, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+    body('tag', 'Tag must not be blank').isLength({ min: 3 }),
+], addArticle)
+
+router.get("/", fetchArticle)
+
+router.put("/edit/:id", UserData, editArticle)
+
+router.delete("/delete/:id", UserData, deleteArticle)
+
+
 
 module.exports = router
