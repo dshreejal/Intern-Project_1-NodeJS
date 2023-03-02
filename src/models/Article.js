7ed15bdf578d8718ccc3228e40721eca05b6befe
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-02 14:18:54 +0545

    Article Model

---
+++ src/models/Article.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ArticleSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+}, { timestamps: true },);
+
+module.exports = mongoose.model("article", ArticleSchema)
