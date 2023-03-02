419af3014f826516bfe2a0ff0eb05caed8d3249f
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-02 15:31:14 +0545

    login controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -40,3 +40,35 @@ exports.registerUser = async (req, res) => {
         console.log(error);
     }
 }
+
+exports.loginUser = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { email, password } = req.body;
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ error: "Please try to login using correct Credentials" })
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(401).json({ error: "Please try to login using correct Credentials" })
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET)
+        const msg = "Logged In successfully"
+        res.json({ authToken, msg })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+}
