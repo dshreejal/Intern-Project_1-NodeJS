dcf081ea44abfacd4f9b5d625e25c37b1c07e899
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-04 18:38:58 +0545

    Added logger function

---
+++ src/utils/logger.js
@@ -0,0 +1,18 @@
+const { createLogger, transports, format } = require('winston');
+
+const Logger = createLogger({
+    transports: [
+        new transports.File({
+            filename: 'src/logs/logEvents.log',
+            level: 'info',
+            format: format.combine(format.timestamp(), format.json())
+        }),
+        new transports.File({
+            filename: 'src/logs/logserrorEvents.log',
+            level: 'error',
+            format: format.combine(format.timestamp(), format.json())
+        }),
+    ]
+});
+
+module.exports = Logger;
