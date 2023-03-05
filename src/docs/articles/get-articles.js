0e97ad2a0918d63e9e2735f6f277e7d7632e8500
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-05 21:46:50 +0545

    some changes

--- src/docs/articles/get-articles.js
+++ src/docs/articles/get-articles.js
@@ -29,7 +29,7 @@ module.exports = {
                     // content-type
                     "application/json": {
                         schema: {
-                            $ref: "#/components/schemas/Article", // Todo model
+                            $ref: "#/components/schemas/Article",
                         },
                     },
                 },
