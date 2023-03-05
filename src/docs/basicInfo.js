594c23d39e50c6a1ddb7f25cbbf5f8a5b270548a
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-05 17:37:47 +0545

    Swagger basic info setup

---
+++ src/docs/basicInfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+    openapi: "3.0.3", // present supported openapi version
+    info: {
+        title: "Simple Backend API", // short title.
+        description: "A simple API to perform CRUD operations", //  desc.
+        version: "0.0.1", // version number
+    },
+}
