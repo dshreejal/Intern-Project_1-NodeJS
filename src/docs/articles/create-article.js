e77eaf1f6279d5f22ee979e79351fe240ef47815
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-05 21:48:07 +0545

    Added add article path

---
+++ src/docs/articles/create-article.js
@@ -0,0 +1,58 @@
+module.exports = {
+    // operation's method
+    post: {
+        tags: ["Article CRUD operations"], // operation's tag
+        description: "Create a new article", // short desc
+        operationId: "createArticle", // unique operation id
+        parameters: [
+            {
+                name: "auth-token",
+                in: 'header',
+                type: 'string',
+                description: "Authorization Token",
+                required: "true",
+            }
+        ], // expected params
+        requestBody: {
+            // expected request body
+            content: {
+                // content-type
+                "application/json": {
+                    schema: {
+                        $ref: "#/components/schemas/AddArticle", // article create data model
+                    },
+                },
+            },
+        },
+        // expected responses
+        responses: {
+            // response code
+            201: {
+                description: "Article created successfully", // response desc
+                content: {
+                    // content-type
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/ArticleOutput", //response output data model
+                        },
+                    },
+                }
+            },
+            400: {
+                description: "Error occured while creating article", // response desc
+                content: {
+                    // content-type
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/ArticleCreateError", //creating error output data model
+                        },
+                    },
+                }
+            },
+            // response code
+            500: {
+                description: "Server error", // response desc
+            },
+        },
+    },
+};
--- src/docs/articles/index.js
+++ src/docs/articles/index.js
@@ -1,11 +1,13 @@
 const getArticles = require('./get-articles');
-
+const createArticle = require('./create-article');
 
 module.exports = {
     paths: {
         '/api/articles': {
             ...getArticles,
         },
-
+        '/api/articles/add': {
+            ...createArticle,
+        }
     }
 }
