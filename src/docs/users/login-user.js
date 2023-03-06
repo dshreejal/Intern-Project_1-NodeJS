59c991ee8101e47d1ab44c41c75564921984c8b4
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-06 19:44:10 +0545

    added register user path

---
+++ src/docs/users/login-user.js
@@ -0,0 +1,58 @@
+module.exports = {
+    // operation's method
+    post: {
+        tags: ["User operations"], // operation's tag
+        description: "User Login", // short desc
+        operationId: "loginUser", // unique operation id
+        parameters: [
+            {
+                name: "auth-token",
+                in: 'header',
+                type: 'string',
+                description: "Authorization Token",
+                required: "true",
+            }
+        ], // expected params
+        requestBody: {
+            // expected request body
+            content: {
+                // content-type
+                "application/json": {
+                    schema: {
+                        $ref: "#/components/schemas/LoginUser", // loginUser  data model
+                    },
+                },
+            },
+        },
+        // expected responses
+        responses: {
+            // response code
+            200: {
+                description: "Logged In successfully", // response desc
+                content: {
+                    // content-type
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/LoginResponse", //response output data model
+                        },
+                    },
+                }
+            },
+            400: {
+                description: "Some fields are not filled properly", // response desc
+                content: {
+                    // content-type
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/LoginUserError", //creating error output data model
+                        },
+                    },
+                }
+            },
+            // response code
+            500: {
+                description: "Server error", // response desc
+            },
+        },
+    },
+};
