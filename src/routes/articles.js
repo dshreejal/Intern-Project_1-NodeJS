c2fdb09d000d7aeac2b115fc159cf5dfccddcb3b
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-07 20:29:32 +0545

    added multer image upload function

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const { body } = require('express-validator');
+const { check } = require('express-validator');
 const UserData = require('../middleware/UserData')
-
+const path = require('path');
 const { addArticle, fetchArticle, editArticle, deleteArticle } = require("../controllers/articles")
 
-router.post("/add", UserData, [
-    body('title', 'Enter a valid title').isLength({ min: 3 }),
-    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/images/'))
+    }, filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/add", UserData, upload.single('img'), [
+    check('title', 'Enter a valid title').isLength({ min: 3 }),
+    check('description', 'Description must be atleast 5 characters').isLength({ min: 5 })
 ], addArticle)
 
 router.get("/", fetchArticle)
 
-router.put("/edit/:id", UserData, editArticle)
+router.put("/edit/:id", UserData, upload.single, editArticle)
 
 router.delete("/delete/:id", UserData, deleteArticle)
 
