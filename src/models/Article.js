3f73f4e0ced829fdcf63d73c800dd76462e04910
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-07 20:31:26 +0545

    added image schema

--- src/models/Article.js
+++ src/models/Article.js
@@ -13,6 +13,9 @@ const ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    img: {
+        type: String,
+    },
     date: {
         type: Date,
         default: Date.now
