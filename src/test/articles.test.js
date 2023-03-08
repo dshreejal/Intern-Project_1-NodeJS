897d5f753915ce3e74881ec503ded75d785210b0
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-08 11:43:21 +0545

    added new test for empty list of articles

--- src/test/articles.test.js
+++ src/test/articles.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-
+const Article = require('../models/Article')
 const app = require('../app');
 
 let server;
@@ -15,6 +15,20 @@ afterAll((done) => {
 });
 
 describe('GET /api/articles', () => {
+    beforeAll(async () => {
+        // Create some sample articles in the database
+        const articles = [
+            { title: 'Article 1', description: 'Body of article 1' },
+            { title: 'Article 2', description: 'Body of article 2' },
+            { title: 'Article 3', description: 'Body of article 3' }
+        ];
+        await Article.insertMany(articles);
+    });
+    afterAll(async () => {
+        // Delete all articles from the database
+        await Article.deleteMany();
+    });
+
     it("should return all articles", async () => {
         const response = await request(app).get('/api/articles');
         expect(response.status).toBe(200);
@@ -22,5 +36,19 @@ describe('GET /api/articles', () => {
         expect(response.body.data.length).toBeGreaterThan(0);
         expect(response.body.message).toBe('Data fetched successfully');
         expect(response.body.error).toBeNull();
-    })
+    });
+
+    it('should return an empty list if there are no articles', async () => {
+        // Delete all articles from the database
+        await Article.deleteMany();
+        const response = await request(app).get('/api/articles');
+        // Check HTTP status code
+        expect(response.status).toBe(200);
+        // Check response body
+
+        expect(response.body.data.length).toBe(0);
+        expect(Array.isArray(response.body.data)).toBe(true);
+        expect(response.body.message).toBe('Data fetched successfully');
+        expect(response.body.error).toBeNull();
+    });
 })
